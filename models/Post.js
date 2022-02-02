9295e271032a97a1a05901d02463c1c53b771c0f
vayne <miguel.osorio19@yahoo.com>
2022-02-02 14:01:53 -0600

    added post method

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create our post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+// we define the post schema
+Post.init(
+    {   
+        // we set the id column as the primary key and set it to auto-increment
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
