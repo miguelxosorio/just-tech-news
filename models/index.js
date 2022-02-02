923828f3d26d8ed114bb1317d7bb107e08380bbb
vayne <miguel.osorio19@yahoo.com>
2022-02-02 16:36:56 -0600

    added CRUD methods for posts

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// user can make many posts - creates one to many relationship
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// but a post can only belong to a user - creates one to one relationship
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Post };
