25c7123ae42cee7f9d658b4874912663b3d710c8
vayne <miguel.osorio19@yahoo.com>
2022-02-09 15:53:53 -0600

    added login feature

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const homeRoutes = require('./home-routes')
+const router = require("express").Router();
+const homeRoutes = require("./home-routes");
 
-const apiRoutes = require('./api');
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const path = require('path');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const path = require("path");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require('express-session');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
@@ -21,5 +34,5 @@ app.use(routes);
 // turn on connection to db and server
 // The "sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
